a4842b8cac63acdf30be981d68d217ab104e57c8
codedchai <guardianx515@gmail.com>
2025-12-02 15:19:58 -0600

    Day 2 part 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -20,5 +20,48 @@ fun main() {
     return invalidIdSum
   }
 
+  fun hasRepeat(id: String, partCount: Int = 2): Boolean {
+    if (partCount > id.length) {
+      return false
+    }
+
+    if (id.length % partCount == 0) {
+      val stringSplitSize = id.length / partCount
+      val stringParts = mutableListOf<String>()
+      var currentIndex = 0
+      while (currentIndex < id.length) {
+        stringParts.add(id.substring(currentIndex, currentIndex + stringSplitSize))
+        currentIndex += stringSplitSize
+      }
+
+      if (stringParts.all { it == stringParts.first() }) {
+        return true
+      }
+    }
+
+    return hasRepeat(id, partCount + 1)
+  }
+
+  fun part2(): Long {
+    val inputLines = readInput("Day02")
+
+    var invalidIdSum = 0L
+
+    inputLines.forEach { inputLine ->
+      val (rangeStart, rangeEnd) = inputLine.split("-")
+      (rangeStart.toLong()..rangeEnd.toLong()).forEach { id ->
+        val idString = id.toString()
+        if (hasRepeat(idString)) {
+          println(id)
+          invalidIdSum += id
+        }
+      }
+    }
+
+
+    return invalidIdSum
+  }
+
   part1().println()
+  part2().println()
 }
