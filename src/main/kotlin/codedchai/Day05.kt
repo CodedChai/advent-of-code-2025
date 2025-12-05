49220506b1e1e8598fbc700130d4cf163153dd83
codedchai <guardianx515@gmail.com>
2025-12-05 16:34:44 -0600

    Day 5 part 2

--- src/main/kotlin/codedchai/Day05.kt
+++ src/main/kotlin/codedchai/Day05.kt
@@ -1,5 +1,8 @@
 package codedchai
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
 
   fun part1(): Int {
@@ -32,5 +35,52 @@ fun main() {
     return freshCount
   }
 
+  fun part2(): Long {
+    val inputLines = readInput("Day05")
+    val ranges = inputLines.mapNotNull { line ->
+      if ("-" !in line) {
+        return@mapNotNull null
+      }
+
+      val (first, second) = line.split("-")
+      first.toLong()..second.toLong()
+    }.toMutableList()
+
+    var anyChanges = true
+    while (anyChanges) {
+      anyChanges = false
+      // easiest to do one by one to avoid oopsies
+      for (index in ranges.indices) {
+        val currentRange = ranges[index]
+        for (innerIndex in ranges.indices) {
+          if (innerIndex == index) {
+            continue
+          }
+          val innerRange = ranges[innerIndex]
+          if (currentRange.first in innerRange || currentRange.last in innerRange) {
+            anyChanges = true
+            ranges.remove(currentRange)
+            ranges.remove(innerRange)
+            val newRange = min(currentRange.first, innerRange.first)..max(currentRange.last, innerRange.last)
+            ranges.add(newRange)
+            break
+          }
+        }
+        if (anyChanges) {
+          break
+        }
+      }
+    }
+
+    var totalFreshProducts = 0L
+
+    ranges.forEach { range ->
+      totalFreshProducts += 1 + (range.last - range.first)
+    }
+
+    return totalFreshProducts
+  }
+
   part1().println()
+  part2().println()
 }
